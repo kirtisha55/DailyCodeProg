8403c900752ab918d942ce820cc06908ea3d2582
Kirti Sharma <ksharma@vitrosoftware.com>
2019-04-17 09:52:34 +1000

    Add project files.

---
+++ ProgramApp/Problem.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramApp
+{
+    class Problem
+    {
+        /// <summary>
+        /// Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+        /// For example, given[10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+
+        public static bool CheckNum(int[] arr, int k)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int j = i + 1;
+                while (j < arr.Length)
+                {
+                    if ((arr[i] + arr[j]) == k)
+                    {
+                        Console.WriteLine(" 2 index - " + i + ", " + j);
+                        return true;
+                    }
+                    j++;
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Given an array of integers, return a new array such that each element at index i of the new array is the product 
+        /// of all the numbers in the original array except the one at i.
+        ///For example, if our input was[1, 2, 3, 4, 5], the expected output would be[120, 60, 40, 30, 24]. 
+        ///If our input was[3, 2, 1], the expected output would be[2, 3, 6].
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
+        //approach 1-> using division
+        public static int[] FindProd1(int[] arr)
+        {
+            List<int> res = new List<int>();
+            int product = 1;
+
+            int prod = arr.Aggregate(1, (a, b) => a * b);
+            Console.WriteLine(" prod val - {0}", prod);
+
+            foreach (var i in arr)
+            {
+                product *= i;
+            }
+
+            for (int j = 0; j < arr.Length; j++)
+            {
+                res.Add(product / arr[j]);
+            }
+
+            return res.ToArray();
+        }
+
+        //approach 2 - without using division
+        public static int[] FindProd2(int[] arr)
+        {
+            List<int> res = new List<int>();
+            int ind = 0;
+            int product = 1;
+            int i = 0;
+            while (ind < arr.Length)
+            {
+                while (i < arr.Length)
+                {
+                    if (i != ind)
+                    {
+                        product *= arr[i];
+                    }
+                    i++;
+                }
+                res.Add(product);
+                product = 1;
+                i = 0;
+                ind++;
+            }
+
+            return res.ToArray();
+        }
+    }
+}
---
+++ ProgramApp/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 10, 15, 3, 7};
+            int num = 17;
+            bool result = Problem.CheckNum(a, num);
+            Console.WriteLine(" Result - " + result);
+            
+            int[] res2 = Problem.FindProd2(new int[] { 1, 2, 3, 4, 5, 0 });
+            int[] res3 = Problem.FindProd2(new int[] { 3,2,1});
+
+            int[] res12 = Problem.FindProd1(new int[] { 1, 2, 3, 4, 5 });
+            int[] res13 = Problem.FindProd1(new int[] { 3, 2, 1 });
+
+            foreach (var n in res2)
+            {
+                Console.WriteLine(" New array  - " + n);
+            }
+            Console.WriteLine("- --------------------- ");
+            foreach (var n in res3)
+            {
+                Console.WriteLine(" New array  - " + n);
+            }
+            Console.WriteLine("==========================");
+            foreach (var n in res12)
+            {
+                Console.WriteLine(" New array 1 - " + n);
+            }
+            Console.WriteLine("- --------------------- ");
+            foreach (var n in res13)
+            {
+                Console.WriteLine(" New array 1 - " + n);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
